deb3f2496013f9ca89242931b704e9ac7b583a5f
DanielRT96 <reti.daniel96@gmail.com>
2020-07-28 17:22:12 +0200

    New Question active

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const API = fetch("http://jservice.io/api/random")
-  .then((response) => response.json())
-  .then((data) => {
-    const answer = data[0].answer;
-    const question = data[0].question;
-    console.log(question);
-    console.log(answer);
-    document.getElementById("question").innerHTML = question;
-  });
+const callAPI = () => {
+  fetch("http://jservice.io/api/random")
+    .then((response) => response.json())
+    .then((data) => {
+      const answer = data[0].answer;
+      const question = data[0].question;
+      console.log(question);
+      console.log(answer);
+      document.getElementById("question").innerHTML = question;
+    });
+};
+
+callAPI();
+
+document.querySelector("#new-btn").addEventListener("click", () => {
+  callAPI();
+});
