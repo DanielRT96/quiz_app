42d77a57449a804fa92e47208c48013e89c91870
DanielRT96 <reti.daniel96@gmail.com>
2020-07-28 19:34:50 +0200

    Minor fixes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btnNext = document.querySelector("#new-btn");
 const addAnswer = document.querySelector("#answer");
 const questionText = document.getElementById("question");
+const selectAnswer = document.getElementById("answer");
 let correctAnswer;
 
 async function callAPI() {
@@ -21,27 +22,28 @@ async function callAPI() {
   }
 }
 
-callAPI().then((data) => {
-  correctAnswer = data[0].answer;
-});
-
-btnNext.addEventListener("click", () => {
-  document.getElementById("answer").value = "";
+function newQuestion() {
   callAPI().then((data) => {
     correctAnswer = data[0].answer;
   });
+}
+btnNext.addEventListener("click", () => {
+  document.getElementById("answer").value = "";
+  newQuestion();
 });
 
 addAnswer.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    const input = document.getElementById("answer").value;
+    const input = selectAnswer.value;
     if (input === correctAnswer) {
+      addAnswer.classList.add("answer.correct");
+      setTimeout(newQuestion(), 100000);
       document.getElementById("answer").value = "";
-      callAPI();
       console.log("correct");
     } else {
       console.log("incorrect");
     }
-    console.log(input);
   }
 });
+
+newQuestion();
