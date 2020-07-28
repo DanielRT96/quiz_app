609f6fb8da46347e4ad52333d335eb396387653d
DanielRT96 <reti.daniel96@gmail.com>
2020-07-28 17:41:44 +0200

    New Question button is done

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ const callAPI = () => {
       const question = data[0].question;
       console.log(question);
       console.log(answer);
+      console.log(data);
       document.getElementById("question").innerHTML = question;
     });
 };
 
 callAPI();
 
+const answerBox = document.querySelector("#answer");
+
 document.querySelector("#new-btn").addEventListener("click", () => {
+  document.getElementById("answer").value = "";
   callAPI();
+});
+
+answerBox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const input = document.getElementById("answer").value;
+
+    input === callAPI.answer ? console.log("correct") : console.log("false");
+
+    console.log(input);
+  }
 });
