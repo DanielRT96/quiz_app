fe49503906e36439f4405b6b96460eded47d5995
DanielRT96 <reti.daniel96@gmail.com>
2020-07-28 16:41:17 +0200

    API call + question update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const API = fetch("http://jservice.io/api/random")
+  .then((response) => response.json())
+  .then((data) => {
+    const answer = data[0].answer;
+    const question = data[0].question;
+    console.log(question);
+    console.log(answer);
+    document.getElementById("question").innerHTML = question;
+  });
